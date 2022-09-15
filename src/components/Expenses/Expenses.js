fc0533537f1bfd17c2a4fb33c4255b884f35cc5c
seoyul <seoyul0203@gmail.com>
2022-09-15 23:53:30 +0900

    ADD: 그래프추가

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2022');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -22,7 +23,8 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         {/* 조건부렌더링, 데이터가 없을경우 */}
-        <ExpensesList data={filtered}  />
+        <ExpensesChart expenses={filtered} />
+        <ExpensesList data={filtered} />
       </Card>
     </div>
   );
